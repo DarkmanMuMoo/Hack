3980325d7ca8b43bb49f90147f82dfd5e28690ee
MuMoo <darkmanmumoonaja@gmail.com>
2016-02-27 20:46:03 +0700

    test

---
+++ src/test/java/com/mumoo/TestCase.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package com.mumoo;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+/**
+ * @author mumoo
+ *
+ */
+public class TestCase {
+
+	@Test
+	public void test() {
+		assertEquals(true, 1 == 1);
+	}
+
+}
